55fd1a6e89664387ce0e09fdd0af9dfa431ac9c2
ivan780 <ivan780@hotmail.es>
2019-07-01 19:58:44 +0200

    fin de la creacion de classe telegrambot(padre) e integracion a la classe holaXTelegram(hijo)

--- model/holaXTelegram.py
+++ model/holaXTelegram.py
@@ -1,27 +1,14 @@
-import requests
 import json
-from model import config
 from model.telegram import TelegramBot
 
 
-class HolaX:
+class HolaX(TelegramBot):
 
     def __init__(self):
-        self.chat_id = None
-        self.text = None
         self.message_text = None
-        self.success = False
         self.check = False
         with open('model/action.json') as json_file:
             self.data = json.load(json_file)
-
-    def set_webhook(self):
-        requests.get(config.TELEGRAM_INIT_WEBHOOK_URL)
-
-    def parse_json(self, data):
-        message = data['message']
-        self.chat_id = message['chat']['id']
-        self.text = message['text']
 
     def action(self):
         for x in self.data['actions']:
@@ -30,9 +17,5 @@ class HolaX:
                 self.check = True
         if self.check:
             self.message_text = 'Hello {}, good morning'.format(self.text)
-        success = self.send_message()
+        success = self.send_message(self.message_text)
         return success
-
-    def send_message(self):
-        res = requests.get(config.TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, self.message_text))
-        return True if res.status_code == 200 else False
--- model/telegram.py
+++ model/telegram.py
@@ -1,10 +1,21 @@
 import requests
 from model import config
+
+
 class TelegramBot:
     def __init__(self):
         self.chat_id = None
-        self.text = 2
+        self.text = None
+        self.success = False
 
-    def setWebhook(self):
-        requests.get()
+    def set_webhook(self):
+        requests.get(config.TELEGRAM_INIT_WEBHOOK_URL)
 
+    def parse_json(self, data):
+        message = data['message']
+        self.chat_id = message['chat']['id']
+        self.text = message['text']
+
+    def send_message(self, message_text):
+        res = requests.get(config.TELEGRAM_SEND_MESSAGE_URL.format(self.chat_id, message_text))
+        return True if res.status_code == 200 else False
